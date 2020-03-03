9951575db352e04780a01c93ede3199f0c73b163
kk6 <hiro.ashiya@gmail.com>
2020-03-04 08:46:23 +0900

    Multiple pages can be output.

--- shimaenaga.py
+++ shimaenaga.py
@@ -1,13 +1,16 @@
 import os
 import pathlib
-from typing import Dict
+from typing import Dict, List
 
 from jinja2 import Environment, FileSystemLoader, Template
 import tomlkit
+import mistune
 
 ROOT_DIR = "."
 TEMPLATE_DIR = "templates"
 DEST_DIR = "dest"
+PAGE_DIR = "pages"
+POST_DIR = "posts"
 
 
 def parse_config(config_file: str) -> Dict:
@@ -20,6 +23,35 @@ def get_template(template_path: pathlib.Path, filename: str) -> Template:
     env = Environment(loader=FileSystemLoader(str(template_path), encoding="utf8"))
     template = env.get_template(filename)
     return template
+
+
+def get_pages(pages_path: pathlib.Path) -> List:
+    page_names = os.listdir(pages_path)
+    pages = []
+    for page in page_names:
+        path = pages_path / page
+        if str(path).endswith(".md"):
+            pages.append(path)
+        else:
+            if path.is_dir():
+                posts = os.listdir(path)
+                for post in posts:
+                    pages.append(path / post)
+    return pages
+
+
+def get_posts(posts_path: pathlib.Path) -> List:
+    _posts = os.listdir(posts_path)
+    posts = []
+    for post in _posts:
+        posts.append(posts_path / post)
+    return posts
+
+
+def parse_markdown(markdown_path: pathlib.Path) -> str:
+    with open(markdown_path) as f:
+        html = mistune.html(f.read())
+    return html
 
 
 def dump_html(dest_dir: pathlib.Path, content: str, filename: str) -> None:
@@ -36,12 +68,20 @@ def main() -> None:
     root_dir = pathlib.Path(dirs.get("root", ROOT_DIR))
     template_dir = root_dir / pathlib.Path(dirs.get("templates", TEMPLATE_DIR))
     dest_dir = root_dir / pathlib.Path(dirs.get("dest", DEST_DIR))
+    pages_dir = root_dir / pathlib.Path(dirs.get("pages", PAGE_DIR))
+    posts_dir = root_dir / pathlib.Path(dirs.get("posts", POST_DIR))
 
     template = get_template(template_dir.resolve(), "index.j2")
 
-    context = {"metadata": config["metadata"], "names": ["john", "alice"]}
-    html = template.render(**context)
-    dump_html(dest_dir, html, "index.html")
+    markdowns = get_pages(pages_dir)
+    posts = get_posts(posts_dir)
+    markdowns.extend(posts)
+    for md in markdowns:
+        name, ext = md.name.split(".md")
+        body = parse_markdown(md)
+        context = {"metadata": config["metadata"], "body": body}
+        html = template.render(**context)
+        dump_html(dest_dir, html, f"{name}.html")
 
 
 if __name__ == "__main__":
